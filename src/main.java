0c6adaf11d05a8d51fb2ea1f8d65afb20ca9f96b
Anna Lapkina <anna.lapkina5@gmail.com>
2022-01-12 20:46:14 +0200

    first commit

---
+++ src/main.java
@@ -0,0 +1,13 @@
+public class main {
+
+    public static void main(String[] args) {
+        int bonusPrice = 20;
+        int ticketPrice = 1200;
+
+        int bonusCount = ticketPrice / bonusPrice;
+
+        System.out.println("Ваши бесплатные мили: " + bonusCount);
+
+
+    }
+}
